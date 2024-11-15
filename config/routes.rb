386eb75465750c76bc9b8cc9967c06a67ddacbcc
Toby Palethorpe <tobypalethorpe@gmail.com>
2024-11-15 12:05:29 +0900

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: 'lists#index'
+
   resources :lists, only: [:new, :create, :index, :show] do
     resources :bookmarks, only: [:new, :create]
   end
